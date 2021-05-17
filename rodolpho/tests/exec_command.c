8e799248469e11f34e8a7c017afeba3f5908e20c
Rodolpho Cammarosano de lima <rcammaro@mi-r3-p4.s19.be>
2021-05-17 18:30:30 +0200

    making some tests on signal handling

--- rodolpho/tests/exec_command.c
+++ rodolpho/tests/exec_command.c
@@ -1,51 +1,52 @@
-
-#include "header.h"
-
-void	free_split(char **array)
-{
-	int	i;
-
-	i = 0;
-	while (array[i])
-		free(array[i++]);
-	free(array);
-}
+#include <stdio.h>
+#include <string.h>
+#include "libft.h"
+#include <signal.h>
 
 void	exec_command(char **argv, char **envp)
 {
 	pid_t	pid_child;
 	int		stat_loc;
-	char	*file;
-
-	file = resolve_path(argv[0]);
 
 	pid_child = fork();
 	if (pid_child == 0)	// child
 	{
-		execve(file, argv, envp);
+		execve(argv[0], argv, envp);
 		printf("%s\n", strerror(errno));
 	}
 	else				// parent process
 		wait(&stat_loc);
-		free(file);
+}
+
+// just ignore the SIGINT signal via a signal handler, as this does not get inherited by an execd process.
+void	signal_handler(int signal_code)
+{
+	(void)signal_code;
 }
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	*prompt = "minishell> ";
+	char	*prompt = "prompt $ ";
 	char	*line;
 	char	**argv_command;
 
-	(void)argc;
-	(void)argv;
+	signal(SIGINT, signal_handler); // ctrl-C
+	signal(SIGQUIT, signal_handler); /* ctrl-\ */
 	write(1, prompt, ft_strlen(prompt));
 	while (get_next_line(0, &line) == 1)
 	{
+		if (ft_strcmp(line, "exit") == 0)
+		{
+			free(line);
+			return (0);
+		}
 		argv_command = ft_split(line, ' ');
 		free(line);
 		exec_command(argv_command, envp);
-		free_split(argv_command);
+		ft_free_split(argv_command);
 		write(1, prompt, ft_strlen(prompt));
 	}
+	(void)argc;
+	(void)argv;
 	return (0);
 }
--- rodolpho/tests/sleeper.c
+++ rodolpho/tests/sleeper.c
@@ -16,5 +16,5 @@ int	main(int argc, char **argv)
 		write(1, "ZzzzZzzz\n", 9);
 		sleep(2);
 	}
-	return (0);
+	return (42);
 }
